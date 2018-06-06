7f5013cb3cb413899bcade28ed5017704018e080
Marwil Campos <marwilcampos@gmail.com>
2018-06-06 17:16:11 -0400

    add joke component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { JokeComponent} from './script';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,11 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    JokeComponent
+  ]
 })
+
+
 export class AppModule { }
